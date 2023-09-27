f76a81d491abbdf31021f5d6a084ce117b911acd
Joao-Dutra <54649142+Joao-Dutra@users.noreply.github.com>
2023-09-26 21:33:18 -0300

    Update EX2_RECURSIVIDADE.java

--- aeds/EX2_RECURSIVIDADE.java
+++ aeds/EX2_RECURSIVIDADE.java
@@ -3,34 +3,28 @@ package aeds;
 import java.util.Random;
 
 public class EX2_RECURSIVIDADE {
-    public static void ExibirArray(int v[],int inicio, int maior){
-        int tamanhovetor = v.length;
-        if (inicio >= tamanhovetor);
-        else{
-            if(v[inicio] > maior) {
-                maior = v[inicio];
-                ExibirArray(v,inicio+1,maior);        
-            }        
-            
-            System.out.println(maior);
-        }    
-        
+    public static int NumMaior(int v[],int inicio, int maior){
+        if (inicio >= v.length) {
+            return maior;
+        }else{
+            if (v[inicio] >= maior) {
+                maior = v[inicio];              
+            }
+            return NumMaior(v,inicio+1,maior);
+        }
     }
     public static void main(String[] args) {
         Random random = new Random();
         int tamanhovetor = 10;
         int v[] = new int [tamanhovetor];
-        int maior = 0;
+        int maior = 0,inicio = 0;
+
         for (int i = 0; i < tamanhovetor; i++) {
             v[i] = random.nextInt(100);
-            if (v[i] > maior) {
-                maior = v[i];
-            }
             System.out.print(v[i] + " ");
         }
-        System.out.print("\n" + maior);
         System.out.println(" ");
-        int inicio = 0;
-        ExibirArray(v,inicio,maior);
+        System.out.print(NumMaior(v,inicio,maior) + "\n");
+        
     }
 }
